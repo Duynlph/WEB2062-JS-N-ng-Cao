257d94d2400fbd66e92057f5368d125f8e22b6fc
unknown <duynlph27635@fpt.edu.vn>
2023-01-12 18:09:48 +0700

    121

---
+++ Section09/121/index.js
@@ -0,0 +1,47 @@
+const restaurant ={
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23,Firenze, Italy',
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+    order: function(starterIndex, mainIndex){
+        return[this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+};
+
+const airline = 'TAP AIR PORTUGAL';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4,7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.indexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function(seat){
+    // B and C are middle seats
+    const s = seat.slice(-1);
+    if(s === 'B' || s === 'E')
+    console.log('you got the middle seat');
+    else console.log('you got lucky');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Jonas'));
